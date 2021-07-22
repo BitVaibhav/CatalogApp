c0d4bb401e3b554c033beb2c0975304a671951c1
Vaibhav <vbhchaurasia001@gmail.com>
2021-07-23 00:09:14 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Pages/home_page.dart';
+import 'package:flutter_application_1/Pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +12,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days course"),
-          ),
-        ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.purple
       ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/" : (context) => LoginPage(),
+        "/home" : (context) => HomePage(),
+        "/login": (context) => LoginPage(),      
+        }
     );
   }
 }
